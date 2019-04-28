493f57d28bf31f9ec57b3868acda85da1a02339d
AngelinaDr <41889153+RedAngelD@users.noreply.github.com>
2019-04-28 23:33:43 +0300

    Add files via upload

---
+++ ClassificationService.svc.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Activation;
+using System.ServiceModel.Web;
+using System.Text;
+using System.Web.Http;
+//using TonalityClassifier;
+using System.IO;
+using Microsoft.ML;
+using Microsoft.ML.Data;
+using static Microsoft.ML.DataOperationsCatalog;
+using Microsoft.ML.Trainers;
+using Microsoft.ML.Transforms.Text;
+using System.Text.RegularExpressions;
+
+namespace ClassificationWebService
+{
+    [ServiceContract(Namespace = "")]
+    [AspNetCompatibilityRequirements(RequirementsMode = AspNetCompatibilityRequirementsMode.Allowed)]
+    public class ClassificationService
+    {
+        // Чтобы использовать протокол HTTP GET, добавьте атрибут [WebGet]. (По умолчанию ResponseFormat имеет значение WebMessageFormat.Json.)
+        // Чтобы создать операцию, возвращающую XML,
+        //     добавьте [WebGet(ResponseFormat=WebMessageFormat.Xml)]
+        //     и включите следующую строку в текст операции:
+        //         WebOperationContext.Current.OutgoingResponse.ContentType = "text/xml";
+        // Добавьте здесь дополнительные операции и отметьте их атрибутом [OperationContract]
+        [OperationContract]
+        public void DoWork()
+        {
+            // Добавьте здесь реализацию операции
+            return;
+        }
+
+        public class SentimentData
+        {
+            [LoadColumn(0)]
+            public string SentimentText;
+
+            [LoadColumn(1), ColumnName("Label")]
+            public bool Sentiment;
+        }
+
+        public class SentimentPrediction
+        {
+            [ColumnName("PredictedLabel")]
+            public bool Prediction { get; set; }
+
+            public float Probability { get; set; }
+
+            public float Score { get; set; }
+        }
+
+        private static readonly string _dataPath = Path.Combine(Environment.CurrentDirectory, "rus_text.txt");
+        public static void GetTonalityClass(string text)
+        {
+            MLContext mlContext = new MLContext();
+
+            //загружаем корпус текстов, получаем наборы для обучения и для тестирования:
+            TrainCatalogBase.TrainTestData splitDataView = LoadData(mlContext);
+
+            //преобразовывем данные из файла и обучаяем нейросеть:
+            ITransformer model = BuildAndTrainModel(mlContext, splitDataView.TrainSet);
+            //оценим созданную нейросеть (получим метрики):
+            //Evaluate(mlContext, model, splitDataView.TestSet);
+            //воспользуемся моделью для классификации тональности обращения:
+            //string tonclass = UseModel(text, mlContext, model);
+            //return tonclass;
+
+        }
+
+        protected static TrainCatalogBase.TrainTestData LoadData(MLContext mlContext)
+        {//ф-я загрузки корпуса текстов, разделения корпуса на наборы данных для обучения и тестирования
+            Microsoft.Data.DataView.IDataView dataView = mlContext.Data.LoadFromTextFile<SentimentData>(_dataPath, hasHeader: false);
+            TrainCatalogBase.TrainTestData splitDataView = mlContext.BinaryClassification.TrainTestSplit(dataView, testFraction: 0.2);
+
+            return splitDataView;
+        }
+
+        protected static ITransformer BuildAndTrainModel(MLContext mlContext, Microsoft.Data.DataView.IDataView splitTrainSet)
+        {//ф-я преобразования данных, обучения нейросети, прогнозирования тональности на основе тестовых данных
+            var estimator = mlContext.Transforms.Text.FeaturizeText(outputColumnName: "Features", inputColumnName: nameof(SentimentData.SentimentText))
+            .Append(mlContext.BinaryClassification.Trainers.FastTree(labelColumnName: "Label", featureColumnName: "Features"));
+            //выбрали алгоритм обучения по дереву принятия решений
+
+            var model = estimator.Fit(splitTrainSet);//обучаем сеть
+            return model;
+        }
+
+        private static List<string> lst = new List<string>
+        {
+            "Arrays",
+            "Queues",
+            "Stacks"
+        };
+
+        [WebGet(UriTemplate = "/Element")]
+        public string GetAllElement() => String.Join(",", lst);
+        /*{
+            System.Net.WebRequest req = System.Net.WebRequest.Create("http://localhost:60051/ClassificationService.svc/Element");
+            req.Method = "POST";
+            req.Timeout = 100000;
+            req.ContentType = "application/json";
+            byte[] sentData = Encoding.GetEncoding(1251).GetBytes("str=Отличная работа");
+            req.ContentLength = sentData.Length;
+            System.IO.Stream sendStream = req.GetRequestStream();
+            sendStream.Write(sentData, 0, sentData.Length);
+            sendStream.Close();
+            //System.Net.WebResponse res = req.GetResponse();
+        }*/
+
+        [WebGet(UriTemplate = "/Element/{ElementId}")]
+        public string GetElementByID(string ElementId)
+        {
+            int pid;
+            if (!Int32.TryParse(ElementId, out pid))
+            {
+                throw new HttpResponseException(HttpStatusCode.BadRequest);
+            }
+            return lst[pid];
+        }
+
+        [WebInvoke(Method = "POST", RequestFormat = WebMessageFormat.Json,
+            UriTemplate = "/Element", ResponseFormat = WebMessageFormat.Json,
+            BodyStyle = WebMessageBodyStyle.Wrapped)]
+        public void AddElement(string str) => GetTonalityClass(str);//lst.Add(TonalClassifier.GetTonalityClass(str));
+
+        [WebInvoke(Method = "DELETE", RequestFormat = WebMessageFormat.Json,
+            UriTemplate = "/Element/{ElementId}", ResponseFormat = WebMessageFormat.Json,
+            BodyStyle = WebMessageBodyStyle.Wrapped)]
+        public void DeleteElement(string ElementId)
+        {
+
+            int pid;
+            if (!Int32.TryParse(ElementId, out pid))
+            {
+                throw new HttpResponseException(HttpStatusCode.BadRequest);
+            }
+            lst.RemoveAt(pid);
+        }
+    }
+}
---
+++ TonalClassifier.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Microsoft.ML;
+using Microsoft.ML.Data;
+using static Microsoft.ML.DataOperationsCatalog;
+using Microsoft.ML.Trainers;
+using Microsoft.ML.Transforms.Text;
+using System.Text.RegularExpressions;
+
+namespace TonalityClassifier
+{
+    public class TonalClassifier
+    {
+        private static readonly string _dataPath = Path.Combine(Environment.CurrentDirectory, "rus_text.txt");
+ 
+        public static string GetTonalityClass(string text)
+        {
+            MLContext mlContext = new MLContext();
+
+            //загружаем корпус текстов, получаем наборы для обучения и для тестирования:
+            TrainCatalogBase.TrainTestData splitDataView = LoadData(mlContext);
+
+            //преобразовывем данные из файла и обучаяем нейросеть:
+            ITransformer model = BuildAndTrainModel(mlContext, splitDataView.TrainSet);
+            //оценим созданную нейросеть (получим метрики):
+            //Evaluate(mlContext, model, splitDataView.TestSet);
+            //воспользуемся моделью для классификации тональности обращения:
+             string tonclass =UseModel(text,mlContext, model);
+             return tonclass;
+
+        }
+
+        protected static TrainCatalogBase.TrainTestData LoadData(MLContext mlContext)
+        {//ф-я загрузки корпуса текстов, разделения корпуса на наборы данных для обучения и тестирования
+            Microsoft.Data.DataView.IDataView dataView = mlContext.Data.LoadFromTextFile<SentimentData>(_dataPath, hasHeader: false);
+            TrainCatalogBase.TrainTestData splitDataView = mlContext.BinaryClassification.TrainTestSplit(dataView, testFraction: 0.2);
+
+            return splitDataView;
+        }
+
+        protected static ITransformer BuildAndTrainModel(MLContext mlContext, Microsoft.Data.DataView.IDataView splitTrainSet)
+        {//ф-я преобразования данных, обучения нейросети, прогнозирования тональности на основе тестовых данных
+            var estimator = mlContext.Transforms.Text.FeaturizeText(outputColumnName: "Features", inputColumnName: nameof(SentimentData.SentimentText))
+            .Append(mlContext.BinaryClassification.Trainers.FastTree(labelColumnName: "Label", featureColumnName: "Features"));
+            //выбрали алгоритм обучения по дереву принятия решений
+
+            var model = estimator.Fit(splitTrainSet);//обучаем сеть
+            return model;
+        }
+
+        protected static void Evaluate(MLContext mlContext, ITransformer model, Microsoft.Data.DataView.IDataView splitTestSet)
+        {//метод загрузки тестового набора данных и оценки нейросети,создание метрик
+            Microsoft.Data.DataView.IDataView predictions = model.Transform(splitTestSet);
+            CalibratedBinaryClassificationMetrics metrics = mlContext.BinaryClassification.Evaluate(predictions, "Label");
+
+            Console.WriteLine();
+            Console.WriteLine("Показатели качества сети:");
+            Console.WriteLine($"Точность: {metrics.Accuracy:P2}");
+            Console.WriteLine($"Баллы: {metrics.F1Score:P2}");
+        }
+
+        protected static string UseModel(string text, MLContext mlContext, ITransformer model)
+        {//тестируем модель
+            PredictionEngine<SentimentData, SentimentPrediction> predictionFunction = mlContext.Model.CreatePredictionEngine<SentimentData, SentimentPrediction>(model);
+            SentimentData sampleStatement = new SentimentData//загружаем наш текст
+            {
+                SentimentText = text
+            };
+            var resultprediction = predictionFunction.Predict(sampleStatement);
+            string tonality_class = (resultprediction.Score > 3) ? "Положительный" : (resultprediction.Score < -3) ? "Отрицательный" : "Нейтральный";
+
+            //Console.WriteLine($"Тестовый текст: {sampleStatement.SentimentText} | Класс тональности:{tonality_class} | Значение: {resultprediction.Score}");
+            //Console.WriteLine();
+            //возвращаем класс тональности исходного текста:
+            return tonality_class;
+        }
+
+    }
+}
+
